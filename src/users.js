add861148f0c0cf5f68068054637b1606d64df1e
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-05-08 15:17:28 +0800

    --

--- src/users.js
+++ src/users.js
@@ -1,9 +1,22 @@
+import { Client, Databases, Query } from "node-appwrite";
+
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
+  const client = new Client()
+    .setEndpoint(process.env.VITE_APPWRITE_URL)
+    .setProject(process.env.VITE_APPWRITE_PROJECT_ID);
+
+  const database = new Databases(client);
+  const posts = database.listDocuments(
+    process.env.VITE_APPWRITE_DATABASE_ID,
+    process.env.VITE_APPWRITE_POST_COLLECTION_ID,
+    Query.limit(4)
+  );
   switch (req.query.type) {
     case "test":
       return res.json(req);
-
+    case "getPosts":
+      return res.json(posts);
     default:
       return res.json({ message: "Hell nah" });
   }
