5e7d24a220f1eb9a9269c8f2c802ef31f936ef13
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-05-08 09:27:35 +0800

    init

--- src/users.js
+++ src/users.js
@@ -1,5 +1,3 @@
-import { Client } from 'node-appwrite';
-
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
@@ -11,23 +9,23 @@ export default async ({ req, res, log, error }) => {
   //    .setKey(process.env.APPWRITE_API_KEY);
 
   // You can log messages to the console
-  log('Hello, Logs!');
+  log("Hello, Logs!");
 
   // If something goes wrong, log an error
-  error('Hello, Errors!');
+  error("Hello, Errors!");
 
   // The `req` object contains the request data
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     // Send a response with the res object helpers
     // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+    return res.send("Hello, World!");
   }
 
   // `res.json()` is a handy helper for sending JSON
   return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+    motto: "Build like a team of hundreds_",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
   });
 };
