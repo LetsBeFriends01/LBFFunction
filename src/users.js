169684b3f0c8f4cd94e4e31733b8ee75ab8ebfda
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-05-08 15:32:50 +0800

    --

--- src/users.js
+++ src/users.js
@@ -1,10 +1,11 @@
-import { appwriteConfig } from "./lib/appwrite";
-
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
   switch (req.query.type) {
     case "test":
-      return res.json({ id: appwriteConfig.databaseId });
+      return res.json({
+        id: process.env.VITE_APPWRITE_PROJECT_ID,
+        name: "gel",
+      });
     case "getPosts":
       return res.json(posts);
     default:
