2a6d46e092664928b2488aaafad3178ec1b82f1d
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-05-08 15:12:47 +0800

    --

--- src/users.js
+++ src/users.js
@@ -1,21 +1,8 @@
-import { Query } from "node-appwrite";
-import { appwriteConfig, database } from "./lib/appwrite";
-
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
   switch (req.query.type) {
     case "test":
       return res.json(req);
-
-    case "getListPost": {
-      const posts = await database.listDocuments(
-        appwriteConfig.databaseId,
-        appwriteConfig.postCollectionId,
-        [Query.limit(10)]
-      );
-
-      return res.json(posts);
-    }
 
     default:
       return res.json({ message: "Hell nah" });
