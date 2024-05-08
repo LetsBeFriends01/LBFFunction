e9460253afe251e3a9043199414cbeca1176771d
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-05-08 18:07:54 +0800

    try env's

--- src/users.ts
+++ src/users.ts
@@ -8,12 +8,23 @@ import {
   Users,
 } from "node-appwrite";
 
+const config = {
+  endpoint: process.env.VITE_APPWRITE_URL || "",
+  projectId: process.env.VITE_APPWRITE_PROJECT_ID || "",
+  apiKey: process.env.APPWRITE_SERVER_ACCESS_AUTH || "",
+
+  databaseId: process.env.VITE_APPWRITE_DATABASE_ID || "",
+
+  // Post Collection
+  postCollectionId: process.env.VITE_APPWRITE_POST_COLLECTION_ID || "",
+};
+
 // It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
+export default async ({ req, res, log, error }: any) => {
   const client = new Client()
-    .setEndpoint(process.env.VITE_APPWRITE_URL)
-    .setProject(process.env.VITE_APPWRITE_PROJECT_ID)
-    .setKey(process.env.APPWRITE_SERVER_ACCESS_AUTH);
+    .setEndpoint(config.endpoint)
+    .setProject(config.projectId)
+    .setKey(config.apiKey);
 
   const database = new Databases(client);
   const storage = new Storage(client);
@@ -22,8 +33,8 @@ export default async ({ req, res, log, error }) => {
   const users = new Users(client);
 
   const posts = await database.listDocuments(
-    process.env.VITE_APPWRITE_DATABASE_ID,
-    process.env.VITE_APPWRITE_POST_COLLECTION_ID,
+    config.databaseId,
+    config.postCollectionId,
     [Query.limit(5)]
   );
 
