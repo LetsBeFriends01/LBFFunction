723e0717b97edf3f147ec4e7d80972937f2a86af
LetsBeFriends01 <letsbefriend.companion@gmail.com>
2024-05-09 13:21:49 +0800

    test post func

--- dist/post.js
+++ dist/post.js
@@ -1,30 +1,51 @@
+var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v, _w, _x;
 import { Account, Avatars, Client, Databases, Storage, Users, } from "node-appwrite";
 const config = {
-    endpoint: process.env.VITE_APPWRITE_URL || "",
-    projectId: process.env.VITE_APPWRITE_PROJECT_ID || "",
-    apiKey: process.env.APPWRITE_SERVER_ACCESS_AUTH || "",
-    databaseId: process.env.VITE_APPWRITE_DATABASE_ID || "",
-    // Post Collection
-    postCollectionId: process.env.VITE_APPWRITE_POST_COLLECTION_ID || "",
+    projectIdL: (_a = process.env.VITE_APPWRITE_PROJECT_ID) !== null && _a !== void 0 ? _a : "",
+    url: (_b = process.env.VITE_APPWRITE_URL) !== null && _b !== void 0 ? _b : "",
+    databaseId: (_c = process.env.VITE_APPWRITE_DATABASE_ID) !== null && _c !== void 0 ? _c : "",
+    storageId: (_d = process.env.VITE_APPWRITE_STORAGE_ID) !== null && _d !== void 0 ? _d : "",
+    userCollectionId: (_e = process.env.VITE_APPWRITE_USER_COLLECTION_ID) !== null && _e !== void 0 ? _e : "",
+    postCollectionId: (_f = process.env.VITE_APPWRITE_POST_COLLECTION_ID) !== null && _f !== void 0 ? _f : "",
+    likedPostCollectionId: (_g = process.env.VITE_APPWRITE_LIKED_POST_COLLECTION_ID) !== null && _g !== void 0 ? _g : "",
+    savesCollectionId: (_h = process.env.VITE_APPWRITE_SAVES_COLLECTION_ID) !== null && _h !== void 0 ? _h : "",
+    // PLANS
+    plansCollectionId: (_j = process.env.VITE_APPWRITE_PLANS_COLLECTION_ID) !== null && _j !== void 0 ? _j : "",
+    planInteractionCollectionId: (_k = process.env.VITE_APPWRITE_PLAN_INTERACTION_COLLECTION_ID) !== null && _k !== void 0 ? _k : "",
+    convoReceiptCollectionId: (_l = process.env.VITE_APPWRITE_CONVERSATION_RECEIPT_COLLECTION_ID) !== null && _l !== void 0 ? _l : "",
+    chatReceiptCollectionId: (_m = process.env.VITE_APPWRITE_CHATS_RECEIPT_COLLECTION_ID) !== null && _m !== void 0 ? _m : "",
+    // CHAT FEATURE
+    messagesStorageId: (_o = process.env.VITE_APPWRITE_MESSAGES_STORAGE_ID) !== null && _o !== void 0 ? _o : "",
+    convoCollectionId: (_p = process.env.VITE_APPWRITE_CONVO_COLLECTION_ID) !== null && _p !== void 0 ? _p : "",
+    chatsCollectionId: (_q = process.env.VITE_APPWRITE_CHATS_COLLECTION_ID) !== null && _q !== void 0 ? _q : "",
+    whoOwnConvoCollectionId: (_r = process.env.VITE_APPWRITE_WHO_OWN_CONVO_COLLECTION_ID) !== null && _r !== void 0 ? _r : "",
+    // VERIFYING
+    verifyingStorageId: (_s = process.env.VITE_APPWRITE_VERIFYING_STORAGE_ID) !== null && _s !== void 0 ? _s : "",
+    verifyingCollectionId: (_t = process.env.VITE_APPWRITE_VERIFYING_COLLECTION_ID) !== null && _t !== void 0 ? _t : "",
+    // REVIEWS
+    reviewsCollectionId: (_u = process.env.VITE_APPWRITE_REVIEWS_COLLECTION_ID) !== null && _u !== void 0 ? _u : "",
+    likedReviewsCollectionId: (_v = process.env.VITE_APPWRITE_LIKED_REVIEWS_COLLECTION_ID) !== null && _v !== void 0 ? _v : "",
+    // FRIEND LIST
+    friendListCollectionId: (_w = process.env.VITE_APPWRITE_FRIEND_LIST_COLLECTION_ID) !== null && _w !== void 0 ? _w : "",
+    // API KEYS
+    serverAccessAuth: (_x = process.env.APPWRITE_SERVER_ACCESS_AUTH) !== null && _x !== void 0 ? _x : "",
 };
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
     const client = new Client()
-        .setEndpoint(config.endpoint)
-        .setProject(config.projectId)
-        .setKey(config.apiKey);
+        .setEndpoint(config.url)
+        .setProject(config.projectIdL)
+        .setKey(config.serverAccessAuth);
     const database = new Databases(client);
     const storage = new Storage(client);
     const account = new Account(client);
     const avatars = new Avatars(client);
     const users = new Users(client);
-    console.log("Payload", req.headers["x-appwrite-event"].includes("delete"));
-    console.log("request", req);
-    log(`This function was called with ${req.method} method`);
-    res.json({ req: req, log: log, error });
+    //   console.log("Payload", req.headers["x-appwrite-event"].includes("delete"));
     if (req.body.whoLiked) {
-        console.log("Payload with conditional", req.body);
-        return res.json({ payload: req.body.whoLiked });
+        if (req.headers["x-appwrite-event"].includes("delete")) {
+            console.log("Yup included", "database ID: ", config.serverAccessAuth);
+        }
     }
     return res.json({ payload: req.body.whoLiked });
 };
--- src/post.ts
+++ src/post.ts
@@ -8,22 +8,63 @@ import {
 } from "node-appwrite";
 
 const config = {
-  endpoint: process.env.VITE_APPWRITE_URL || "",
-  projectId: process.env.VITE_APPWRITE_PROJECT_ID || "",
-  apiKey: process.env.APPWRITE_SERVER_ACCESS_AUTH || "",
+  projectIdL: process.env.VITE_APPWRITE_PROJECT_ID ?? "",
+  url: process.env.VITE_APPWRITE_URL ?? "",
+  databaseId: process.env.VITE_APPWRITE_DATABASE_ID ?? "",
+  storageId: process.env.VITE_APPWRITE_STORAGE_ID ?? "",
+  userCollectionId: process.env.VITE_APPWRITE_USER_COLLECTION_ID ?? "",
+  postCollectionId: process.env.VITE_APPWRITE_POST_COLLECTION_ID ?? "",
+  likedPostCollectionId:
+    process.env.VITE_APPWRITE_LIKED_POST_COLLECTION_ID ?? "",
+  savesCollectionId: process.env.VITE_APPWRITE_SAVES_COLLECTION_ID ?? "",
 
-  databaseId: process.env.VITE_APPWRITE_DATABASE_ID || "",
+  // PLANS
 
-  // Post Collection
-  postCollectionId: process.env.VITE_APPWRITE_POST_COLLECTION_ID || "",
+  plansCollectionId: process.env.VITE_APPWRITE_PLANS_COLLECTION_ID ?? "",
+  planInteractionCollectionId:
+    process.env.VITE_APPWRITE_PLAN_INTERACTION_COLLECTION_ID ?? "",
+  convoReceiptCollectionId:
+    process.env.VITE_APPWRITE_CONVERSATION_RECEIPT_COLLECTION_ID ?? "",
+  chatReceiptCollectionId:
+    process.env.VITE_APPWRITE_CHATS_RECEIPT_COLLECTION_ID ?? "",
+
+  // CHAT FEATURE
+
+  messagesStorageId: process.env.VITE_APPWRITE_MESSAGES_STORAGE_ID ?? "",
+  convoCollectionId: process.env.VITE_APPWRITE_CONVO_COLLECTION_ID ?? "",
+  chatsCollectionId: process.env.VITE_APPWRITE_CHATS_COLLECTION_ID ?? "",
+  whoOwnConvoCollectionId:
+    process.env.VITE_APPWRITE_WHO_OWN_CONVO_COLLECTION_ID ?? "",
+
+  // VERIFYING
+
+  verifyingStorageId: process.env.VITE_APPWRITE_VERIFYING_STORAGE_ID ?? "",
+  verifyingCollectionId:
+    process.env.VITE_APPWRITE_VERIFYING_COLLECTION_ID ?? "",
+
+  // REVIEWS
+
+  reviewsCollectionId: process.env.VITE_APPWRITE_REVIEWS_COLLECTION_ID ?? "",
+
+  likedReviewsCollectionId:
+    process.env.VITE_APPWRITE_LIKED_REVIEWS_COLLECTION_ID ?? "",
+
+  // FRIEND LIST
+
+  friendListCollectionId:
+    process.env.VITE_APPWRITE_FRIEND_LIST_COLLECTION_ID ?? "",
+
+  // API KEYS
+
+  serverAccessAuth: process.env.APPWRITE_SERVER_ACCESS_AUTH ?? "",
 };
 
 // It's executed each time we get a request
 export default async ({ req, res, log, error }: any) => {
   const client = new Client()
-    .setEndpoint(config.endpoint)
-    .setProject(config.projectId)
-    .setKey(config.apiKey);
+    .setEndpoint(config.url)
+    .setProject(config.projectIdL)
+    .setKey(config.serverAccessAuth);
 
   const database = new Databases(client);
   const storage = new Storage(client);
@@ -31,14 +72,12 @@ export default async ({ req, res, log, error }: any) => {
   const avatars = new Avatars(client);
   const users = new Users(client);
 
-  console.log("Payload", req.headers["x-appwrite-event"].includes("delete"));
-  console.log("request", req);
-  log(`This function was called with ${req.method} method`);
-  res.json({ req: req, log: log, error });
+  //   console.log("Payload", req.headers["x-appwrite-event"].includes("delete"));
 
   if (req.body.whoLiked) {
-    console.log("Payload with conditional", req.body);
-    return res.json({ payload: req.body.whoLiked });
+    if (req.headers["x-appwrite-event"].includes("delete")) {
+      console.log("Yup included", "database ID: ", config.serverAccessAuth);
+    }
   }
 
   return res.json({ payload: req.body.whoLiked });
